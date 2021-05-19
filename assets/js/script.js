92df2a23e56361c99135557921d3418dee134ca4
brickdwarf <81526068+brickdwarf@users.noreply.github.com>
2021-05-18 21:10:29 -0400

    Experimenting with modules, pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+/* PSUEDO CODING SCRATCHPAD
+Hook listener to button to initiate quiz
+On start, logic picks a question and feeds it to the renderer
+Select a question using a random number, check if questions 
+    have already been asked (Array includes?)
+Once selected, interface draws question/choices to appropriate boxes (randomized as well?)
+On click, the contents of the selection are compared to correct answer, pass/fail determined
+User feedback given and another question is drawn (flash elements red/green briefly?)
+    Possibly shuffle missed questions back into the stack?
+    Deplete timer (check for 0)
+Timer counts down seconds, checking for 0
+Timer reaches 0, call fail function
+    Clear elements, indicate failure. Offer restart.
+    Restart clears question stack, begins again.
+All questions answered, freeze timer
+*/
+
+
+/*=======================================
+// Quiz Logic Module
+------------------------------------------
+Handles the backend logic of the quiz app
+=========================================*/
+var quizLogic = (function() {
+
+    return {
+        testMethod : function() {
+            console.log("It worked!");
+        }
+    };
+})();
+
+/*====================================
+// Quiz Interface Module
+------------------------------------------
+Interfaces with the DOM and the GUI
+====================================*/
+var quizInterface = (function() {
+
+})();
+
+/*====================================
+// Runtime Code
+------------------------------------------
+Initiates the app
+====================================*/
+
+quizLogic.testMethod();
